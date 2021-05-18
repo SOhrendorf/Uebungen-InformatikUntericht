0f6b8dce70d65a5f926eabc1fffd84585c238978
Simon <S.Ohrendorf@web.de>
2021-05-18 14:23:28 +0200

    Initial sharing of project

---
+++ Braut.java
@@ -0,0 +1,33 @@
+
+/**
+ * Äquivalent zu Brautigam
+ * 
+ * @author Simon Ohrendorf 
+ * @version V.0.1 20.05.2021
+ */
+public class Braut
+{
+    // Instanzvariablen
+    protected String vorname;
+    protected String nachname;
+    protected int persoNr;
+    protected boolean verheiratet;
+    protected int hochzeitstag;
+    protected boolean ring;
+    
+
+    /**
+     * Konstruktor für Objekte der Klasse Braut
+     */
+    public Braut(String pVorname, String pNachname, int pPersoNr, int pHochzeitstag)
+    {
+        // Instanzvariable initialisieren
+        vorname = pVorname; 
+        nachname = pNachname;
+        persoNr = pPersoNr;
+        verheiratet = false;
+        hochzeitstag = pHochzeitstag;
+        ring = false;
+    }
+    
+}
